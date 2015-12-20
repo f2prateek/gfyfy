d69ebee99be3f78203496645b9334f4313222a5d
Prateek Srivastava <f2prateek@gmail.com>
2015-12-20 14:24:24 -0800

    Refactor

--- cmd/gfyfy/main.go
+++ cmd/gfyfy/main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"net/url"
 	"os"
 
 	"github.com/f2prateek/gfyfy/Godeps/_workspace/src/github.com/tj/docopt"
@@ -41,45 +42,62 @@ func gfycatURL(url string) string {
 	return fmt.Sprintf("http://upload.gfycat.com/transcode?fetchUrl=%s", url)
 }
 
+func serve(w http.ResponseWriter, r *http.Request) {
+	gifURL := r.URL.Path[1:]
+	if gifURL == "" {
+		w.WriteHeader(200)
+		fmt.Fprintln(w, http.StatusText(200))
+		return
+	}
+	if gifURL == "favicon.ico" {
+		http.NotFound(w, r)
+		return
+	}
+	if _, err := url.Parse(gifURL); err != nil {
+		http.Error(w, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
+		return
+	}
+	logger := log.New(os.Stderr, gifURL+" ", log.LstdFlags)
+	gfycatURL := gfycatURL(gifURL)
+	logger.Println("gfycat url", gfycatURL)
+	resp, err := http.Get(gfycatURL)
+	if err != nil {
+		logger.Println("error fetching gfycat response", err)
+		http.Error(w, http.StatusText(http.StatusBadGateway), http.StatusBadGateway)
+		return
+	}
+	var gyfycatResponse GyfycatResponse
+	if err := json.NewDecoder(resp.Body).Decode(&gyfycatResponse); err != nil {
+		logger.Println("error decoding gfycat response", err)
+		http.Error(w, http.StatusText(http.StatusBadGateway), http.StatusBadGateway)
+		return
+	}
+	if gyfycatResponse.WebmURL == "" {
+		logger.Println("no webmUrl returned")
+		http.Error(w, http.StatusText(http.StatusBadGateway), http.StatusBadGateway)
+		return
+	}
+	logger.Println("redirecting to", gyfycatResponse.WebmURL)
+	http.Redirect(w, r, gyfycatResponse.WebmURL, http.StatusMovedPermanently)
+}
+
 func main() {
 	args, err := docopt.Parse(usage, nil, true, version, false)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		url := r.URL.Path[1:]
-		logger := log.New(os.Stderr, url+" ", log.LstdFlags)
-		gfycatURL := gfycatURL(url)
-		logger.Println("gfycat url", gfycatURL)
-		resp, err := http.Get(gfycatURL)
-		if err != nil {
-			logger.Println("error fetching gfycat response", err)
-			http.Error(w, http.StatusText(http.StatusBadGateway), http.StatusBadGateway)
-			return
-		}
-		var gyfycatResponse GyfycatResponse
-		if err := json.NewDecoder(resp.Body).Decode(&gyfycatResponse); err != nil {
-			logger.Println("error decoding gfycat response", err)
-			http.Error(w, http.StatusText(http.StatusBadGateway), http.StatusBadGateway)
-			return
-		}
-		if gyfycatResponse.WebmURL == "" {
-			logger.Println("no webmUrl returned")
-			http.Error(w, http.StatusText(http.StatusBadGateway), http.StatusBadGateway)
-			return
-		}
-		logger.Println("redirecting to", gyfycatResponse.WebmURL)
-		http.Redirect(w, r, gyfycatResponse.WebmURL, http.StatusMovedPermanently)
-	})
-
 	addr := args["--addr"].(string)
 	if addr == ":8080" {
 		envPort := os.Getenv("PORT")
 		if envPort != "" {
+			log.Println("using $PORT", envPort)
 			addr = ":" + envPort
 		}
 	}
-	log.Println("starting server on", addr)
-	log.Fatal(http.ListenAndServe(":8080", nil))
+
+	log.Println("starting gfyfy server on", addr)
+	if err := http.ListenAndServe(addr, http.HandlerFunc(serve)); err != nil {
+		log.Fatal(err)
+	}
 }
